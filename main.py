9ebb9a362df181ae75e7e412ab24120ecbee9b51
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2023-10-10 21:11:27 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import urllib.request
+import urllib.parse
+import random
+
+# 步数默认96000 至97999 随机数，可以自行修改。
+# 接口限制，最高不得超过 98000。
+
+step = random.randint(10000, 11000)
+# 账号
+user = "408683724@qq.com"
+# 密码
+password = "abc123456"
+
+
+
+def main():
+    base_url = "https://apis.jxcxin.cn/api/mi?"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+        'host': 'apis.jxcxin.cn',
+        'Accept': '*/*',
+    }
+    data = {
+        "user": user,
+        "password": password,
+        'step': step,
+        'ver': 'cxydzsv3.1',
+    }
+    new_data = urllib.parse.urlencode(data)
+    url = base_url + new_data
+    request = urllib.request.Request(url=url, headers=headers)
+    response = urllib.request.urlopen(request)
+    content = response.read().decode("utf-8")
+    print(content)
+
+
+# 腾讯云函数
+def main_handler(event, context):
+    return main()
+
+
+# 阿里云函数
+def handler(event, context):
+    return main()
+
+
+if __name__ == '__main__':
+    main()
