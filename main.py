25d4a30dc56073c57caa2bd328305c08e094f059
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2024-09-09 08:56:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -63,10 +63,10 @@ def login(user, password):
         data2 = {
             "allow_registration=": "false",
             "app_name": "com.xiaomi.hm.health",
-            "app_version": "6.3.5",
+            "app_version": "6.12.0",
             "code": f"{code}",
             "country_code": "CN",
-            "device_id": "2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
+            "device_id": "2C8B4939-0CCB-4E94-8CBA-CB8EA6E614A1",
             "device_model": "phone",
             "dn": "api-user.huami.com%2Capi-mifit.huami.com%2Capp-analytics.huami.com",
             "grant_type": "access_token",
